93dc8062a6d387da3262648e9c8e648a98801dbb
Derrie N <ewenaiwe@gmail.com>
2019-09-25 10:22:38 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,35 @@
 class Board
   
- attr_accessor :cells
+  attr_accessor :cells
  
  
- def initialize
-  self.cells = Array.new(9, " ")
- end
+  def initialize
+    self.cells = Array.new(9, " ")
+  end
+  
+  def turn_count
+    self.cells.count{|token| token == "X" || token == "O"}
+    binding.pry
+  end
  
- def reset!
-   self.cells = Array.new(9, " ")
+  def full?
+    self.cells.all? {|cell| cell != " "}
+  end
+ 
+  def position(input)
+    self.cells[input.to_i.-1]
+  end
+ 
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+ 
+  def reset!
+    self.cells = Array.new(9, " ")
   end
   
 end
