b8e25aa8ae65c8252f1be817544631ab5e584fe0
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-29 17:21:25 -0400

    Added: Command line interface with options to play with 0-2 human players

--- lib/game.rb
+++ lib/game.rb
@@ -64,12 +64,59 @@ class Game
       @board.display
       turn
     end
+    @board.display
 
     if won?
       puts "Congratulations #{winner}!"
     else
       puts "Cat's Game!"
     end
+  end
+
+  def self.start
+
+    puts "Welcome to Tic Tac Toe"
+
+    number_of_players = "A"
+
+    while !number_of_players.to_i.between?(0,2) || !!number_of_players.match(/\D/) || number_of_players == ""
+
+      print "How many Human Players?: "
+      number_of_players = gets.chomp
+
+    end
+
+    number_of_players = number_of_players.to_i
+
+    if number_of_players == 0
+      player_1 = Players::Computer.new("X")
+      player_2 = Players::Computer.new("O")
+
+    elsif number_of_players == 1
+      print "Should the Human or Computer Go First?(h or c):"
+      input = ""
+      until input == "h" || input == "c"
+        input = gets.chomp.downcase
+      end
+
+      if input == "h"
+        player_1 = Players::Human.new("X")
+        player_2 = Players::Computer.new("O")
+
+      else
+        player_1 = Players::Computer.new("X")
+        player_2 = Players::Human.new("O")
+
+      end
+
+    elsif number_of_players == 2
+      player_1 = Players::Human.new("X")
+      player_2 = Players::Human.new("O")
+
+    end
+
+    self.new(player_1, player_2).play
+
   end
 
 end
