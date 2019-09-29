e3bb0e09ed69fb2fbcbab568e7c6884aa4738820
clonex10100 <clonex10100@gmail.com>
2019-09-29 19:58:25 -0400

    Cleaned up cli a bit

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,7 @@ class Game
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
-    @board = board
-  end
+    @board = board end
 
   def current_player
     board.turn_count.even? ? player_1 : player_2
@@ -33,6 +32,7 @@ class Game
   def play
     while !over?
       @board.display
+      puts "$$$$$$$$$$$"
       turn
     end
     @board.display
@@ -45,16 +45,28 @@ class Game
   end
 
   def self.start
-
     puts "Welcome to Tic Tac Toe"
+    input = nil
 
-    number_of_players = "A"
+    while !input || (input != "y" && input != "n")
+      puts "Would you like to watch wargame mode? y/n"
+      input = gets.strip.downcase
+    end
 
-    while !number_of_players.to_i.between?(0,2) || !!number_of_players.match(/\D/) || number_of_players == ""
+    if input == "y"
+      play_wargame
+    else
+      play_game
+    end
+  end
 
+  private
+  def self.play_game
+    number_of_players = nil
+
+    while !number_of_players || !number_of_players.to_i.between?(0,2) || !!number_of_players.match(/\D/)
       print "How many Human Players?: "
       number_of_players = gets.chomp
-
     end
 
     number_of_players = number_of_players.to_i
@@ -65,7 +77,7 @@ class Game
 
     elsif number_of_players == 1
       print "Should the Human or Computer Go First?(h or c):"
-      input = ""
+      input = nil
       until input == "h" || input == "c"
         input = gets.chomp.downcase
       end
@@ -73,21 +85,43 @@ class Game
       if input == "h"
         player_1 = Players::Human.new("X")
         player_2 = Players::Computer.new("O")
-
       else
         player_1 = Players::Computer.new("X")
         player_2 = Players::Human.new("O")
-
       end
 
     elsif number_of_players == 2
       player_1 = Players::Human.new("X")
       player_2 = Players::Human.new("O")
-
     end
 
     self.new(player_1, player_2).play
+  end
 
+  def self.play_wargame
+      times = nil
+      while !times || (times.match(/\D/) || !(times.to_i > 0))
+        puts "How many games would you like to run?"
+        times = gets.strip
+      end
+
+      res = times.to_i.times.reduce({w: 0, l: 0, d: 0}) do |track, i|
+        g = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
+        g.play
+        if g.won?
+          if g.winner == x
+            track[:w] += 1
+          else
+            track[:l] += 1
+          end
+        else
+          track[:d] += 1
+        end
+        track
+      end
+      puts "Player 1 won #{res[:w]}"
+      puts "Player 2 won #{res[:l]}"
+      puts "#{res[:d]} ties"
   end
 
 end
